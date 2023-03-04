b03c20be90211f9d8abf7fd2c6ff8a8b84131223
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2023-03-04 12:11:12 -0300

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+const Genre = require('./Genre');
+const Director = require('./Director');
+const Movie = require('./Movie');
+const Actor = require('./Actor')
+
+
+Movie.belongsToMany(Actor,{through:"movieActor"});
+Actor.belongsToMany(Movie,{through:"movieActor"});
+
+Movie.belongsToMany(Genre,{through:"movieGenre"});
+Genre.belongsToMany(Movie,{through:"movieGenre"});
+
+Movie.belongsToMany(Director,{through:"movieDirector"});
+Director.belongsToMany(Movie,{through:"movieDirector"});
